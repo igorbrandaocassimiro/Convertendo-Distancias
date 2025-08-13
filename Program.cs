9a4397019a8b2eeb1d3a2b85077de41d14518b6e
Igor Brandão <igorbrandaocassimirodaflon@gmail.com>
2025-08-13 16:19:02 -0300

    Primeiro Commit do Projeto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] arge)
+    {
+        double milhas = 10.0; 
+
+        double quilometros = milhas * 1.60934; 
+
+        Console.WriteLine(milhas + " milhas equivalem a " + quilometros + " quilômetros."); 
+    }
+}
+
+
+
+
+
+
+
